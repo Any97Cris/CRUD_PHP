359bfb3a9fb016a40c396949937c935be7a7aa00
Any97Cris <criscianysilva1997@gmail.com>
2022-12-23 14:18:22 -0400

    criacao de campos

--- adicionar_action.php
+++ adicionar_action.php
@@ -6,16 +6,23 @@ require_once 'dao/UsuarioDAO.php';
 $usuarioDAO = new UsuarioDAU($pdo);
 
 $name = filter_input(INPUT_POST, 'nome');
-$sobrenome = filter_input(INPUT_POST, 'sobrenome');
+$login = filter_input(INPUT_POST, 'login');
 $email = filter_input(INPUT_POST, 'email');
+$telefone = filter_input(INPUT_POST, 'telefone');
+$status = filter_input(INPUT_POST, 'status');
 
-if ($name && $sobrenome && $email) {
+// var_dump($status);
+// exit;
+
+if ($name && $login && $email) {
 
     if ($usuarioDAO->findByEmail($email) === false) {
         $novoUsuario = new Usuario();
         $novoUsuario->setNome($name);
-        $novoUsuario->setSobrenome($sobrenome);
+        $novoUsuario->setLogin($login);
         $novoUsuario->setEmail($email);
+        $novoUsuario->setTelefone($telefone);
+        $novoUsuario->setStatus($status);
 
         $usuarioDAO->add($novoUsuario);
 
