aebe57071a429d7028950bdf288d6b918dce4fa4
Any97Cris <criscianysilva1997@gmail.com>
2022-12-23 14:35:00 -0400

    acrescentei e alterei o nome dos campos

--- editar.php
+++ editar.php
@@ -27,7 +27,7 @@ if ($usuario === false) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="src/style.css">
     <title>Editar</title>
 </head>
 
@@ -38,18 +38,32 @@ if ($usuario === false) {
             <h2 class="mb-5">Editar Usuário</h2>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Nome</label>
-                <input type="text" name="nome" value="<?= $usuario->getNome(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1" placeholder="Nome">
+                <input type="text" name="nome" value="<?= $usuario->getNome(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1">
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Sobrenome</label>
-                <input type="text" name="sobrenome" value="<?= $usuario->getSobrenome(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1" placeholder="Sobrenome">
+                <input type="text" name="login" value="<?= $usuario->getLogin(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email</label>
+                <input type="email" name="email" value="<?= $usuario->getEmail(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Telefone</label>
+                <input type="text" required name="telefone" value="<?= $usuario->getTelefone(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1" maxLength="11">
             </div>
             <div class="mb-5">
-                <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <input type="email" name="email" value="<?= $usuario->getEmail(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1" placeholder="fulano@exemplo.com">
+                <label for="exampleFormControlInput1" class="form-label">Status</label>
+                <input type="text" required name="status" value="<?= $usuario->getStatus(); ?>" class="form-control" style="width: 40%;" id="exampleFormControlInput1" maxLength="1">
+                <span class="legenda">
+                    <ul>
+                        <li>A = Ativo</li>
+                        <li>I = Inativo</li>
+                    </ul>
+                </span>
             </div>
             <!--BOTÕES-->
-            <div style="display:inline-block;">
+            <div style="display:inline-block;" class="mb-5">
                 <input class="btn btn-warning mr-2" type="submit" value="Editar">
                 <a class="btn btn-secondary" href="index.php">Menu</a>
             </div>
